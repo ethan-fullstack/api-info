157a018077735b57211f4f2ca9dfc9bacb3ce73f
Ethan <estebanh.fullstack@gmail.com>
2023-01-27 12:51:13 -0500

    Uploads folder

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-ENV PORT=9001
-EXPOSE 9001
+ENV PORT=9777
+EXPOSE 9777
 
 CMD [ "node", "." ]
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const { readFile } = require("fs");
 const moment = require("moment");
 const app = express();
-const port = 9001;
+const port = 9777;
 
 const today = new Date();
 const msg = "update and deploy: ok!! final";
+
+const path = require("path");
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.get("/", (req, res) => {
   res.json({
@@ -12,6 +16,16 @@ app.get("/", (req, res) => {
     status: "ok",
     msg: msg,
     moment: moment().format("LLLL"),
+  });
+});
+
+app.get("/home", (req, res) => {
+  console.log(__dirname);
+  readFile("./home.html", "utf-8", (err, html) => {
+    if (err) {
+      res.send("Service currenty unavailable");
+    }
+    res.send(html);
   });
 });
 
